5c2646b3f69051db9e80657884cd100d7729bf11
Maykel <jotasenator@gmail.com>
2022-12-09 13:33:55 -0500

    implement router version 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { Navigation } from "./routes/Navigation";
 
 function App() {
   return (
     <>
       <h1>App.tsx</h1>
+      <Navigation />
     </>
   );
 }
