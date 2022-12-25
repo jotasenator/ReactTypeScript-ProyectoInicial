1d4a5f4aacafee283b1c49e7830302df13748985
Maykel <jotasenator@gmail.com>
2022-12-25 01:36:27 -0500

    Configuracion y muestra que no estan configurados lso compoenentes ni controlados

--- src/02-component-patters/pages/ShopingPage.tsx
+++ src/02-component-patters/pages/ShopingPage.tsx
@@ -1,18 +1,28 @@
 import coffeMug from "../../../public/coffee-mug.png";
+import coffeMugMeme from "../../../public/coffee-mug2.png";
 import {
   ProductCard,
   ProductImage,
   ProductTitle,
   ProductButtons,
 } from "../components";
+import { Product } from "../interfaces/interfaces";
 
 import "../styles/custom-styles.css";
 
-const product = {
+const product1 = {
   id: "1",
   title: "title from product",
   img: coffeMug,
 };
+
+const product2 = {
+  id: "2",
+  title: "Coffee Mug Meme",
+  img: coffeMugMeme,
+};
+
+const products: Product[] = [product1, product2];
 
 const ShopingPage = () => {
   return (
@@ -26,7 +36,58 @@ const ShopingPage = () => {
           flexWrap: "wrap",
         }}
       >
-        <ProductCard product={product} style={{ background: "#70D1F8" }}>
+        {products.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className="bg-dark text-white"
+          >
+            <ProductImage
+              className="custom-image"
+              style={{
+                boxShadow: "10px 10px 10px black",
+              }}
+            />
+            <ProductTitle className="text-bold" />
+            <ProductButtons className="custom-buttons" />
+          </ProductCard>
+        ))}
+
+        <div className="shopping-cart">
+          <ProductCard
+            product={product2}
+            className="bg-dark text-white"
+            style={{
+              width: "100px",
+            }}
+          >
+            <ProductImage
+              className="custom-image"
+              style={{
+                boxShadow: "10px 10px 10px black",
+              }}
+            />
+            <ProductButtons className="custom-buttons" />
+          </ProductCard>
+
+          <ProductCard
+            product={product1}
+            className="bg-dark text-white"
+            style={{
+              width: "100px",
+            }}
+          >
+            <ProductImage
+              className="custom-image"
+              style={{
+                boxShadow: "10px 10px 10px black",
+              }}
+            />
+            <ProductButtons className="custom-buttons" />
+          </ProductCard>
+        </div>
+
+        {/* <ProductCard product={product1} style={{ background: "#70D1F8" }}>
           <ProductImage
             style={{
               boxShadow: "10px 10px 10px black",
@@ -46,17 +107,23 @@ const ShopingPage = () => {
           />
         </ProductCard>
 
-        <ProductCard product={product} className="bg-dark">
+        <ProductCard product={product1} className="bg-dark">
           <ProductImage className="custom-image" />
           <ProductTitle className="text-white" />
           <ProductButtons className="custom-buttons" />
         </ProductCard>
 
-        <ProductCard product={product} className="bg-dark">
+        <ProductCard product={product2} className="bg-dark">
+          <ProductImage className="custom-image" />
+          <ProductTitle className="text-white" />
+          <ProductButtons className="custom-buttons" />
+        </ProductCard> */}
+
+        {/* <ProductCard product={product} className="bg-dark">
           <ProductCard.Image className="custom-image" />
           <ProductCard.Title className="text-white" />
           <ProductCard.Buttons className="custom-buttons" />
-        </ProductCard>
+        </ProductCard> */}
       </div>
     </div>
   );
