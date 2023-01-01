e9527b1eafef5edf41027955126893472cba8218
Maykel <jotasenator@gmail.com>
2023-01-01 18:32:30 -0500

    Expandir funcionalidad del custom hook

--- src/03-forms/hooks/useForm.ts
+++ src/03-forms/hooks/useForm.ts
@@ -10,8 +10,19 @@ export const useForm = <T>(initState: T) => {
     }));
   };
 
+  const isValidEmail = (email: string) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  const resetForm = () => {
+    setFormData({ ...initState })
+  }
+
   return {
     ...formData,
-    onChange
+    isValidEmail,
+    onChange,
+    resetForm,
   }
 }
--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -2,7 +2,15 @@ import { useForm } from "../hooks/useForm";
 import "../styles/styles.css";
 
 export const RegisterPage = () => {
-  const { name, email, password1, password2, onChange } = useForm({
+  const {
+    name,
+    email,
+    password1,
+    password2,
+    onChange,
+    resetForm,
+    isValidEmail,
+  } = useForm({
     name: "",
     email: "",
     password1: "",
@@ -19,19 +27,23 @@ export const RegisterPage = () => {
 
       <form noValidate onSubmit={onSubmit}>
         <input
+          className={`${name.trim().length <= 0 && "has-error"}`}
           name="name"
           onChange={onChange}
           placeholder="Name"
           type="text"
           value={name}
         />
+        {name.trim().length <= 0 && <span>Mandatory field *</span>}
         <input
+          className={`${!isValidEmail(email) && "has-error"}`}
           name="email"
           onChange={onChange}
           placeholder="Email"
           type="email"
           value={email}
         />
+        {!isValidEmail(email) && <span>Mandatory field *</span>}
         <input
           name="password1"
           onChange={onChange}
@@ -39,6 +51,10 @@ export const RegisterPage = () => {
           type="password"
           value={password1}
         />
+        {password1.trim().length <= 0 && <span>Mandatory field *</span>}
+        {password1.trim().length < 6 && password1.trim().length > 0 && (
+          <span>Password should be bigger than 5 characters *</span>
+        )}
         <input
           name="password2"
           onChange={onChange}
@@ -46,6 +62,10 @@ export const RegisterPage = () => {
           type="password"
           value={password2}
         />
+        {password2.trim().length <= 0 && <span>Mandatory field *</span>}
+        {password2.trim().length > 0 && password1 !== password2 && (
+          <span>Passwords provided must be identical *</span>
+        )}
         <button
           type="submit"
           onClick={() =>
@@ -53,6 +73,9 @@ export const RegisterPage = () => {
           }
         >
           Create
+        </button>
+        <button type="button" onClick={resetForm}>
+          Reset Form
         </button>
       </form>
     </div>
